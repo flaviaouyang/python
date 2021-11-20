811f89a18738ba188aade4767366ef10cafac887
Flavia Ouyang <flavia.ouyang@mail.mcgill.ca>
2021-11-19 22:46:44 -0500

    added total amount count

--- source_code/grocery.py
+++ source_code/grocery.py
@@ -1,6 +1,5 @@
 grocery = {}
 
-
 def print_grocery(grocery):
     print("Grocery List: ")
     for k, v in grocery.items():
@@ -12,7 +11,11 @@ def add_to_grocery(grocery, add_item):
         grocery.setdefault(item, 0)
         grocery[item] += 1
     print_grocery(grocery)
+    total = 0
+    for v in grocery.values():
+        total += v
+    print( "Total items:", total)
 
 
-today = ['apple', 'banana', 'brie', 'chips', 'beer', 'apple', 'apple']
+today = ['apple', 'banana', 'brie', 'chips', 'beer', 'apple', 'apple', 'brie']
 add_to_grocery(grocery, today)
