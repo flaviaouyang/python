60dd59f6d194698350f25eaae6525863a0ffe884
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-04-29 16:55:12 -0400

    reorganize repo

--- source-code/hello_world.py
+++
@@ -1,5 +0,0 @@
-name = input("What's your name, please?\n")
-greeting = name + " says, \"Hello, World!\""
-print(greeting)
-age = int(input("How old are you, please?\n"))
-print(name + " is " + str(age) + " years old.")
