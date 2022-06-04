5b4e56d9c73f45791a746610ceb99334562b6a7d
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-06-04 18:50:54 -0400

    Update list example

--- basics/data-structure/list.py
+++ basics/data-structure/list.py
@@ -3,9 +3,26 @@ zoo = ["lion", "tiger", "crocodile"]
 
 print(pet_shop[0])
 print(pet_shop[2:])
+print(pet_shop[-1])
+print(pet_shop[:2])
+
+print("""
+--------------------------------------------------------
+      """)
 
 animal = pet_shop.copy()
 animal.extend(zoo)
 
 print(pet_shop)
 print(animal)
+
+# output:
+# dog
+# ['rabbit', 'snake', 'hamster']
+# hamster
+# ['dog', 'cat']
+
+# --------------------------------------------------------
+      
+# ['dog', 'cat', 'rabbit', 'snake', 'hamster']
+# ['dog', 'cat', 'rabbit', 'snake', 'hamster', 'lion', 'tiger', 'crocodile']
