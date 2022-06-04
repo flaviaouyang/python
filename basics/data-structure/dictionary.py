3092fe5cd6070b84c76a371dc2fbcefcc7e061d1
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-06-04 18:37:51 -0400

    Add dictionary example

---
+++ basics/data-structure/dictionary.py
@@ -0,0 +1,23 @@
+month = {
+    "Jan": 1,
+    "Feb": 2,
+    "Mar": 3,
+    "Apr": 4,
+    "May": 5,
+    "Jun": 6,
+    "Jul": 7,
+    "Aug": 8,
+    "Sep": 9,
+    "Oct": 10,
+    "Nov": 11,
+    "Dec": 12,
+}
+
+print(month)
+print(month["Apr"])
+# output: 4
+
+for key, value in month.items():
+  print(key)
+  print(value)
+
