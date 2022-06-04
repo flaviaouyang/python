04655486bdbd21b809183ab6b138f6ca077bee4a
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-06-04 18:51:36 -0400

    Update list example

--- basics/data-structure/list.py
+++ basics/data-structure/list.py
@@ -2,9 +2,14 @@ pet_shop = ["dog", "cat", "rabbit", "snake", "hamster"]
 zoo = ["lion", "tiger", "crocodile"]
 
 print(pet_shop[0])
-print(pet_shop[2:])
+
+print(pet_shop[2:]) 
+# inclusive
+
 print(pet_shop[-1])
+
 print(pet_shop[:2])
+# exclusive
 
 print("""
 --------------------------------------------------------
