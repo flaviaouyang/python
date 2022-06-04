f516b87adb4ee4e828e1f765f462df5776a988fe
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-06-04 19:12:21 -0400

    Add example for enumerate

---
+++ basics/enumerate_function.py
@@ -0,0 +1,15 @@
+courses = ["history", "philosophy", "geography", "calculus"]
+
+for index, course in enumerate(courses, start=1):
+  print("Course number: ", index)
+  print("Course: ", course)
+  
+# output:
+# Course number:  1
+# Course:  history
+# Course number:  2
+# Course:  philosophy
+# Course number:  3
+# Course:  geography
+# Course number:  4
+# Course:  calculus
